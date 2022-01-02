5d1c77bf052a03bb41e7e8ff6276bebbaa03a54a
GothNoire <ruzanow.n@yandex.ru>
2022-01-02 18:39:14 +0400

    some changes

--- DownloadTaxiModule.py
+++ DownloadTaxiModule.py
@@ -1,13 +1,24 @@
+import csv
+
 import requests
 import luigi
+from luigi.contrib import sqla
+import pandas as pd
+from typing import List
+from sqlalchemy import Numeric, Date
+from luigi.util import requires
+from dateutil.relativedelta import relativedelta
+import init_events
+
+def get_filename(year: int, month: int) -> str:
+    return f'yellow_tripdata_{year}-{month:02}.csv'
 
 class DownloadTaxiTripPark(luigi.Task):
-    year = luigi.Parameter()
-    month = luigi.Parameter()
+    date = luigi.MonthParameter()
 
     @property
-    def filename(self):
-        return 'yellow_tripdata_'+self.year+'-'+self.month+'.csv'
+    def filename(self): 
+        return get_filename(self.date.year, self.date.month)
 
     def download_data (self, filename: str) -> requests.Response:
         url = f'https://s3.amazonaws.com/nyc-tlc/trip+data/{filename}'
@@ -22,3 +33,52 @@ class DownloadTaxiTripPark(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget('taxi_file/'+self.filename)
+
+@requires(DownloadTaxiTripPark)
+class AggregateTaxiTrip(luigi.Task):
+    def group_by_date(self, file, group_by='date', metrics: List[str] = None) -> pd.DataFrame:
+        dframe = pd.read_csv(file)
+        dframe[group_by] = pd.to_datetime(dframe['tpep_pickup_datetime']).dt.strftime('%Y-%m-%d')
+        dframe = dframe.groupby(group_by)[metrics].sum().reset_index()
+        return dframe
+
+    @property
+    def filename(self):
+        return get_filename(self.date.year, self.date.month)
+
+    def run(self):
+        with self.input().open() as first_file, self.output().open('w') as aggr_file:
+            dframe = self.group_by_date (file=first_file, metrics=['passenger_count', 'trip_distance', 'total_amount'])
+            dframe.to_csv(aggr_file.name, index=False)
+
+    def output(self):
+        return luigi.LocalTarget('taxi_file/aggr_'+self.filename)
+
+@requires(AggregateTaxiTrip)
+class CopyAgrrInfoToSQLliteBBd(sqla.CopyToTable):
+
+    table = 'yellow_NY_trip'
+    connection_string = 'sqlite:///sqlite.db'
+
+    columns = [
+        (['date', Date()], {}),
+        (['passenger_count', Numeric(2)], {}),
+        (['trip_distance', Numeric(2)], {}),
+        (['total_amount', Numeric(2)], {}),
+    ]
+
+    def rows(self):
+        with self.input().open() as file:
+            df = pd.read_csv(file, parse_dates=[0])
+            rows = df.to_dict(orient='split')['data']
+            return rows
+
+class YellowTaxiDateRangeTask(luigi.WrapperTask):
+    start = luigi.MonthParameter()
+    stop = luigi.MonthParameter()
+
+    def requires(self):
+        current_month = self.start
+        while current_month <= self.stop:
+            yield CopyAgrrInfoToSQLliteBBd(date=current_month)
+            current_month += relativedelta(months=1)
---
+++ TelegramNotification.py
@@ -0,0 +1,29 @@
+import DownloadTaxiModule
+import telebot
+import traceback
+from luigi import Task
+import DownloadTaxiModule
+import glob
+
+chat_id = ['554900514','962581764','1327219370','404163509']
+bot = telebot.TeleBot('5078862539:AAFDie49p_T0lbeOzOp0NHda_3u4tMokFZI')
+
+def send_notif (message_text: str):
+    for rec in chat_id:
+        bot.send_message(rec, message_text)
+
+def set_err_tlg_notif(task: Task, exception: Exception):
+    message_text = f'Сломався таск:{task.__class__.__name__}' + f' Исключение:{exception.__class__.__name__} '+f' Трейс:{traceback.format_exc()}'
+    send_notif (message_text)
+
+def set_start_tlg_notif(task: Task):
+    message_text = f'Запустився таск: {task.__class__.__name__}'
+    send_notif(message_text)
+
+def set_succes_tlg_notif(task: Task):
+    message_text = f'Таска {task.__class__.__name__} выполнилась успешно! '
+    send_notif (message_text)
+    if task.__class__.__name__ == 'YellowTaxiDateRangeTask':
+        for rec in chat_id:
+            for file in glob.glob('taxi_file/aggr_yellow_tripdata_*.csv'):
+                bot.send_document(rec, open(file, 'rb'))
